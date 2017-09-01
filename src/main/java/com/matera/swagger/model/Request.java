003cee3a4ee93137614e07abfe515cbc87a8a28c
Altieres de Matos <altieres.matos@matera.com>
2017-09-01 15:57:13 -0300

    News fields in request

--- src/main/java/com/matera/swagger/model/Request.java
+++ src/main/java/com/matera/swagger/model/Request.java
@@ -2,19 +2,26 @@ package com.matera.swagger.model;
 
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.common.collect.ImmutableSet;
 
 public class Request {
 
-	private String url;
 	private String uri;
-	private Set<Header> headers;
-	private Set<PathParam> pathParams;
-	private Set<QueryParam> queryParams;
+	private String baseUrl;
+	private String url;
+	private Set<Header> headers = ImmutableSet.of();
+	private Set<PathParam> pathParams = ImmutableSet.of();
+	private Set<QueryParam> queryParams = ImmutableSet.of();
 	private String body;
 
 	public String getUri() {
 		return this.uri;
+	}
+
+	public String getBaseUrl() {
+		return this.baseUrl;
 	}
 
 	public String getUrl() {
@@ -36,23 +43,24 @@ public class Request {
 	public String getBody() {
 		return this.body;
 	}
+	
 
 	public static class RequestBuilder {
 
 		private Request request = new Request();
-
-		public RequestBuilder withUrl(String url) {
-			request.url = url;
-			return this;
+		
+		public Request build() {
+			request.buildUrl();
+			request.validateBaseUrl();
+			request.validateUri();
+			request.validatePathParam();
+			request.parseParam();
+			return request;
 		}
 
 		public RequestBuilder withUri(String uri) {
 			request.uri = uri;
 			return this;
-		}
-
-		public Request build() {
-			return request;
 		}
 
 		public RequestBuilder withHeaders(Header... header) {
@@ -74,6 +82,45 @@ public class Request {
 			request.body = body;
 			return this;
 		}
+
+		public RequestBuilder withBaseUrl(String baseUrl) {
+			request.baseUrl = baseUrl;
+			return this;
+		}
+	}
+
+	private void buildUrl() {
+		this.url = StringUtils.join(this.baseUrl, this.uri);
+	}
+
+	private void validatePathParam() {
+		for (PathParam pathParam : pathParams) {
+			String search = String.format("{%s}", pathParam.getName());
+			boolean contains = StringUtils.contains(this.uri, search);
+			if (!contains) {
+				String message = String.format("The path param %s was not aware in uri.", search);
+				throw new IllegalArgumentException(message);
+			}
+		}
+	}
+
+	private void parseParam() {
+		for (PathParam pathParam : pathParams) {
+			String replace = String.format("\\{%s\\}", pathParam.getName());
+			this.url = StringUtils.replaceAll(this.url, replace, pathParam.getValue());
+		}
+	}
+
+	private void validateBaseUrl() {
+		if (StringUtils.isBlank(this.baseUrl)) {
+			throw new IllegalArgumentException("The baseUrl value cannot be null or empty.");
+		}		
+	}
+	
+	private void validateUri() {
+		if (StringUtils.isBlank(this.uri)) {
+			throw new IllegalArgumentException("The uri value cannot be null or empty.");
+		}		
 	}
 
 }
--- src/test/java/com/matera/swagger/model/RequestTest.java
+++ src/test/java/com/matera/swagger/model/RequestTest.java
@@ -1,10 +1,10 @@
 package com.matera.swagger.model;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.google.common.collect.ImmutableSet;
-import com.matera.swagger.model.Header;
-import com.matera.swagger.model.Request;
 import com.matera.swagger.model.Request.RequestBuilder;
 
 import junit.framework.Assert;
@@ -12,23 +12,26 @@ import junit.framework.Assert;
 public class RequestTest {
 
 	private Request request;
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	@Test
 	public void shouldGetUri() {
-		request = new RequestBuilder().withUri("/v1/my-first-uri").build();
-		Assert.assertEquals("/v1/my-first-uri", request.getUri());
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").build();
+		Assert.assertEquals("/api/v1/my-first-uri", request.getUri());
 	}
 
 	@Test
-	public void shouldGetUrl() {
-		request = new RequestBuilder().withUrl("http://localhost:8080/api").build();
-		Assert.assertEquals("http://localhost:8080/api", request.getUrl());
+	public void shouldGetBaseUrl() {
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").build();
+		Assert.assertEquals("http://localhost:8080", request.getBaseUrl());
 	}
 	
 	@Test
 	public void shouldGetOnlyOneHeader() {
 		Header header = new Header("name", "value");
-		request = new RequestBuilder().withHeaders(header).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withHeaders(header).build();
 		Assert.assertEquals(ImmutableSet.of(header), request.getHeaders());
 	}
 	
@@ -36,29 +39,29 @@ public class RequestTest {
 	public void shouldGetManyHeaders() {
 		Header headerOne = new Header("name", "value");
 		Header headerTwo = new Header("name", "value");
-		request = new RequestBuilder().withHeaders(headerOne, headerTwo).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withHeaders(headerOne, headerTwo).build();
 		Assert.assertEquals(ImmutableSet.of(headerOne, headerTwo), request.getHeaders());
 	}
 	
 	@Test
 	public void shouldGetOnlyOnePathParam() {
 		PathParam pathParam = new PathParam("name", "value");
-		request = new RequestBuilder().withPathParam(pathParam).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri/{name}").withPathParam(pathParam).build();
 		Assert.assertEquals(ImmutableSet.of(pathParam), request.getPathParams());
 	}
 	
 	@Test
 	public void shouldGetManyPathParam() {
-		PathParam pathParamOne = new PathParam("name", "value");
-		PathParam pathParamTwo = new PathParam("name", "value");
-		request = new RequestBuilder().withPathParam(pathParamOne, pathParamTwo).build();
+		PathParam pathParamOne = new PathParam("param-one", "param-one-value");
+		PathParam pathParamTwo = new PathParam("param-two", "param-two-value");
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri/{param-one}/{param-two}").withPathParam(pathParamOne, pathParamTwo).build();
 		Assert.assertEquals(ImmutableSet.of(pathParamOne, pathParamTwo), request.getPathParams());
 	}
 	
 	@Test
 	public void shouldGetOnlyOneQueryParam() {
 		QueryParam queryParam = new QueryParam("name", "value");
-		request = new RequestBuilder().withQueryParam(queryParam).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withQueryParam(queryParam).build();
 		Assert.assertEquals(ImmutableSet.of(queryParam), request.getQueryParams());
 	}
 	
@@ -66,15 +69,50 @@ public class RequestTest {
 	public void shouldGetManyQueryParam() {
 		QueryParam queryParamOne = new QueryParam("name", "value");
 		QueryParam queryParamTwo = new QueryParam("name", "value");
-		request = new RequestBuilder().withQueryParam(queryParamOne, queryParamTwo).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withQueryParam(queryParamOne, queryParamTwo).build();
 		Assert.assertEquals(ImmutableSet.of(queryParamOne, queryParamTwo), request.getQueryParams());
 	}
 	
 	@Test
 	public void shouldGetBody() {
 		String json = "{\"key\":\"value\"}";
-		request = new RequestBuilder().withBody(json).build();
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withBody(json).build();
 		Assert.assertEquals(json, request.getBody());
+	}
+	
+	@Test
+	public void shouldGetUrl() {
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").build();
+		Assert.assertEquals("http://localhost:8080/api/v1/my-first-uri", request.getUrl());
+	}
+	
+	@Test
+	public void shouldValidateMandatoryBashUrl() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("The baseUrl value cannot be null or empty.");
+		request = new RequestBuilder().withUri("/api/v1/my-first-uri").build();
+	}
+	
+	@Test
+	public void shouldValidateMandatoryUri() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("The uri value cannot be null or empty.");
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").build();
+	}
+	
+	@Test
+	public void shouldParsePathParams() {
+		PathParam pathParam = new PathParam("some-param", "some-param-value");
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri/{some-param}").withPathParam(pathParam).build();
+		Assert.assertEquals("http://localhost:8080/api/v1/my-first-uri/some-param-value", request.getUrl());
+	}
+	
+	@Test
+	public void shouldValidateAmountPathParams() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("The path param {some-param} was not aware in uri.");
+		PathParam pathParam = new PathParam("some-param", "some-param-value");
+		request = new RequestBuilder().withBaseUrl("http://localhost:8080").withUri("/api/v1/my-first-uri").withPathParam(pathParam).build();
 	}
 	
 }
